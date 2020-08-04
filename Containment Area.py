66b2c4ed081ef709c97ff1a4e3a5ed2eb67850a6
El Drago <zsb.eldrago@gmail.com>
2020-08-04 17:40:02 +0100

    / ‘Containment Area.py’

--- Containment Area.py
+++ Containment Area.py
@@ -1,6 +1,9 @@
+#This is an area to test code.
 print("This code was not created by a cryptographic expert and has not been reviewed.")
 print("Under no circumstances should it be used for real world perposes")
 
 def encrypt(PublicKey, Max, plaintext):
     for character in plaintext:
         print(character)
+
+encrypt("1","1",input())
