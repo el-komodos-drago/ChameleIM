88209d0a50aa844835c7553e9f4d8ea85be8d140
El Drago <zsb.eldrago@gmail.com>
2020-08-11 22:37:51 +0100

    / ‘Containment Area.py’

--- Containment Area.py
+++ Containment Area.py
@@ -160,7 +160,7 @@ def decrypt(PrivateKey, Max, ciphertext):
 # cipher = encrypt(83,7471,input())
 # print(decrypt(347,7471,cipher))
 # print(Num2Text(decrypt(347,7471,cipher)))
-random = SystemRandom().randint(70000,1000000)
+random = SystemRandom().randint(257,1000000)
 print(random)
 prime = RandomPrime(random)
 print(prime)
