ac23f9c6bf814d967ab2e5a8ed3c1370560f9a86
El Drago <zsb.eldrago@gmail.com>
2020-08-12 18:13:32 +0100

    / ‘Containment Area.py’

--- Containment Area.py
+++ Containment Area.py
@@ -156,7 +156,7 @@ def decrypt(PrivateKey, Max, ciphertext):
 #####  END OF CRYPTOGRAPHIC CODE  #####
 #######################################
 
-prime1 = RandomPrime(secrets.SystemRandom.randrange(65537,1000000))
+prime1 = RandomPrime(SystemRandom.randrange(65537,1000000))
 
 Max, PublicKey, PrivateKey = (GenerateKeypair(646253, 93383))
 
