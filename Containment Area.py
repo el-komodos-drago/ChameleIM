205c0c53ce0d22ddc74d61c497b76652be0180b1
El Drago <zsb.eldrago@gmail.com>
2020-08-14 16:21:13 +0100

    / ‘Containment Area.py’

--- Containment Area.py
+++ Containment Area.py
@@ -36,6 +36,7 @@ def RandomPrime(cap):
     print("2")
     for itteration in range(2, int(sqrt(cap)+1)):
         if prime[itteration] == True: #if itteration is prime...
+            print("3")
             for i in range(itteration*2, cap+1, itteration):
                 prime[i] = False #mark all multiples of itteration as not prime
     print("as")
