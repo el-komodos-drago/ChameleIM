c668fb0ed89b3f2d9fab91757422d9b3f7d3c059
El Drago <zsb.eldrago@gmail.com>
2020-08-14 16:23:39 +0100

    / ‘Containment Area.py’

--- Containment Area.py
+++ Containment Area.py
@@ -161,8 +161,8 @@ def decrypt(PrivateKey, Max, ciphertext):
 #####  END OF CRYPTOGRAPHIC CODE  #####
 #######################################
 
-prime1 = RandomPrime(SystemRandom().randint(257,10000000))
-prime2 = RandomPrime(SystemRandom().randint(257,10000000))
+prime1 = RandomPrime(SystemRandom().randint(257,100000000))
+prime2 = RandomPrime(SystemRandom().randint(257,100000000))
 
 print(prime1, prime2)
 Max, PublicKey, PrivateKey = (GenerateKeypair(prime1, prime2))
