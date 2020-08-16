b76fd8edb7a8a4fabd4802e13d228eb91cab5624
El Drago <zsb.eldrago@gmail.com>
2020-08-16 17:26:54 +0100

    / ‘Containment Area.py’

--- Containment Area.py
+++ Containment Area.py
@@ -6,7 +6,7 @@ from math import sqrt
 from random import randrange
 from secrets import SystemRandom
 import time
-from cryptography import number
+from cryptography.utils import number
 
 def Num2Text (numbers):
     text = ""
