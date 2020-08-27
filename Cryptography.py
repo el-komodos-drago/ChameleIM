d496e202c8429725e9e66d31116479c31ea503b6
El Drago <zsb.eldrago@gmail.com>
2020-08-27 14:15:30 +0100

    / ‘Cryptography.py’

--- Cryptography.py
+++ Cryptography.py
@@ -128,6 +128,7 @@ def Hash(data, salt = urandom(16)): #hash string with a salt that defaults to 16
     return(HashResult.decode("ANSI"),salt.decode("ANSI"))
 
 t0 = time.time()
-random.SystemRandom().randrange(1000,10000000000)
+rn = random.SystemRandom().randrange(1000,10000000000)
 t1 = time.time()
 print (t0 - t1)
+print(rn)
