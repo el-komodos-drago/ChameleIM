edfd59c0f9973e529086ed08c0f271c0e9ff2567
El Drago <zsb.eldrago@gmail.com>
2020-09-09 18:11:18 +0100

    / ‘GUI.py’

--- GUI.py
+++ GUI.py
@@ -93,6 +93,7 @@ header.grid(row=0, column=0, columnspan=3,sticky="ew")
 MainWindow.rowconfigure(1,weight=1) #resize the buttom row
 #Column 1
 MainWindow.columnconfigure(0, weight=1,minsize=150) #resize ContactBar
+MainWindow.update_idletasks()
 print(MainWindow.winfo_width())
 ContactBar = RenderContactBar()
 
