754b1ef20a5d8ac47fd718e2c0d3848456eb55d4
El Drago <zsb.eldrago@gmail.com>
2020-10-29 12:53:43 +0000

    / ‘GUI 3.py’

--- GUI 3.py
+++ GUI 3.py
@@ -108,7 +108,7 @@ def DisplayContact(ContactID):
                                          bg="Dark Grey", relief = tkinter.FLAT,
                                          font="Arial 10 bold")
         MessageFromLabel.pack(anchor="w")
-        MessageImage = tkinter.PhotoImage(file="messages/"+message[1]+".png")
+        MessageImage = tkinter.PhotoImage(file="messages/"+str(message[1])+".png")
         
         MessageText = GetMessageText(message[1], message[3],message[2])
         MessageTextLabel = tkinter.Label(master=MessageFrame,text=MessageText,
