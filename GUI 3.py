b5a4eb0b648458cf7739031ca702d7346f568d27
El Drago <zsb.eldrago@gmail.com>
2020-10-30 16:01:41 +0000

    / ‘GUI 3.py’

--- GUI 3.py
+++ GUI 3.py
@@ -247,15 +247,16 @@ def CreateMessageList():
 
     MessageListScrollBox.configure(yscrollcommand=MLScrollBar.set)
 
-    MessageListScrollBox.pack(padx = 15, pady = 10, side="left", fill="both", expand=True)
+    MessageListScrollBox.pack(padx = 15, pady = 10, side="top", fill="both", expand=True)
     MLScrollBar.pack(side="right", fill="y")
     
     #Send Message Box
     SendMessageFrame = tkinter.Frame(MessageList, bg = "white", highlightbackground = "black",
                                      highlightthickness = 1)
     SendMessageFrame.pack(side="bottom",anchor="s")
-    SendMessageButton = tkinter.Button(SendMessageFrame, bg = "white", text="✈")
-    SendMessageButton.pack(anchor="e")
+    SendMessageButton = tkinter.Button(SendMessageFrame, bg = "white", relief="flat",
+                                        text="✈")
+    SendMessageButton.pack(anchor="e",padx = 10, pady = 10)
 
 def Resize(details):
     MaxCharacters = (details.width/50)-2
