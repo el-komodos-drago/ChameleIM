f8e55a0a9ae7136a81d88231df183c580e357548
El Drago <zsb.eldrago@gmail.com>
2020-11-05 17:17:52 +0000

    / ‘Post Image.py’

--- Post Image.py
+++ Post Image.py
@@ -1,5 +1,5 @@
 # Simple example to post an image file to the server
 import requests
-url='http://34.123.25.148:8080/upload/image.gif'
-files={'image':open('C:\logonew.gif','rb')}
+url='http://34.123.25.148:8080/upload/image.png'
+files={'image':open('C:\Sample.png','rb')}
 r=requests.post(url, files=files)
